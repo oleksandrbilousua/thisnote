3c35a64fcaa87bf873e1d8ccc3de93d7c9dc5cca
Oleksandr Bilous <oleksandr.bilous.ua@gmail.com>
2024-02-07 21:31:52 +0200

    fix build

--- app/src/main/java/com/example/thisnote/data/db/NoteDatabase.kt
+++ app/src/main/java/com/example/thisnote/data/db/NoteDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.thisnote.data.Note
 
-@Database(entities = [Note::class], version = 2)
+@Database(entities = [Note::class], version = 2, exportSchema = false)
 abstract class NoteDatabase : RoomDatabase() {
     abstract val noteDao: NoteDao
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id ("com.google.dagger.hilt.android") version "2.50" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.20.0"
 }
 buildscript {
     dependencies {
